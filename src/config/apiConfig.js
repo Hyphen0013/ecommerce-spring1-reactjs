efe8c841dd087dd78f016a62d24ce2ae910567ad
Hyphen0013 <hyphen.call@gmail.com>
2023-08-23 21:38:41 +0530

    online backend server url"

--- src/config/apiConfig.js
+++ src/config/apiConfig.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
-export const API_BASE_URL = "http://localhost:8080";
+// export const API_BASE_URL = "http://localhost:8080";
+export const API_BASE_URL = "https://ecommerce-spring1-production.up.railway.app/";
 
 const jwt = localStorage.getItem("hyphen_JWT");
 
